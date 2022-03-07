7343a3ca8a65764b04b443bead18c5252a66d2f3
Lucas Sussermann <sussermann@gmail.com>
2022-03-07 13:17:29 -0300

    CRUD Endpoints created

--- product-crud-api/Controllers/ProductController.cs
+++ product-crud-api/Controllers/ProductController.cs
@@ -19,5 +19,51 @@ namespace product_crud_api.Controllers
         {
             return _context.Products.ToList();
         }
+
+        public async Task<Product> Get(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        [HttpPost]
+        public int Post(Product product)
+        {
+            if (product.Id != 0)
+                return 0;
+
+            var addedProductId = _context.Products.Add(product).Entity.Id;
+            _context.SaveChanges();
+
+            return addedProductId;
+        }
+
+        [HttpPut]
+        public int Put(Product product)
+        {
+            if (product.Id == 0)
+                return 0;
+
+            var updatedProductId = _context.Products.Update(product).Entity.Id;
+            _context.SaveChanges();
+
+            return updatedProductId;
+        }
+
+        [HttpDelete]
+        public async Task<int> Delete(int id)
+        {
+            if (id == 0)
+                return 0;
+
+            var productToDelete = await _context.Products.FindAsync(id);
+
+            if (productToDelete == null)
+                return 0;
+
+            _context.Remove(productToDelete);
+            _context.SaveChanges();
+            return productToDelete.Id;
+        }
+
     }
 }
